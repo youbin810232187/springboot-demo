33c6cd1c5cea83ad82307fed761ee59b66504417
DirectorU <810232187@qq.com>
2019-03-18 19:13:27 +0800

    测试提交

---
+++ src/main/java/com/youbin/springboot/aspect/LogAspect.java
@@ -0,0 +1,32 @@
+package com.youbin.springboot.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+
+@Component
+@Aspect
+public class LogAspect {
+    private  Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    @Around("execution(* com.youbin.springboot.controller.HelloController.*(..))")
+    public Object simpleLog(ProceedingJoinPoint pjp){
+        Object o = null;
+        try {
+            Object[] args = pjp.getArgs();
+            logger.info(args.toString());
+            o = pjp.proceed();
+            logger.info("return:" + o.toString());
+            return o;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return  o;
+    }
+}
